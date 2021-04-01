78d66f7175e85a3d72c9d6ce1ddabcc85e27423a
ningjiangzhu <979207315@qq.com>
2021-04-01 14:55:34 +0800

    main modify app.py

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def test_url_for():
     print(url_for('user_page', name='peter'))  # 输出：/user/peter
     print(url_for('test_url_for'))  # 输出：/test
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    print(url_for('test_url_for', num=3))  # 输出：/test?num=2
     return 'Test page'
